6c15182e45169b7b90e35e7854f330b01f043493
laminemessaci <lamine.messaci18@gmail.com>
2022-11-11 17:17:47 +0100

    Now BlogPosts page don't need usEffect, useState , loadin..

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
 import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
+import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
 import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
+import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index path="/" element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route path=":id" element={<PostDetailPage />} />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -1,36 +1,48 @@
 import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Posts from '../components/Posts';
 import { getPosts } from '../util/api';
 
 function BlogPostsPage() {
-  const [error, setError] = useState();
-  const [posts, setPosts] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  /**
+   * Returns Data coming from loader function declared below
+   *
+   * @return  {[Promise]}  return posts
+   */
+  const loaderData = useLoaderData();
+  // const [error, setError] = useState();
+  // const [posts, setPosts] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    async function loadPosts() {
-      setIsLoading(true);
-      try {
-        const posts = await getPosts();
-        setPosts(posts);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
+  // useEffect(() => {
+  //   async function loadPosts() {
+  //     setIsLoading(true);
+  //     try {
+  //       const posts = await getPosts();
+  //       setPosts(posts);
+  //     } catch (err) {
+  //       setError(err.message);
+  //     }
+  //     setIsLoading(false);
+  //   }
 
-    loadPosts();
-  }, []);
+  //   loadPosts();
+  // }, []);
 
   return (
     <>
-      <h1>Our Blog Posts</h1>
+      {/* <h1>Our Blog Posts</h1>
       {isLoading && <p>Loading posts...</p>}
       {error && <p>{error}</p>}
-      {!error && posts && <Posts blogPosts={posts} />}
+      {!error && posts && <Posts blogPosts={posts} />} */}
+      <Posts blogPosts={loaderData} />
     </>
   );
 }
 
 export default BlogPostsPage;
+
+export function loader() {
+  return getPosts();
+}
