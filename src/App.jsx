ba82674791ac5ee26089c92814023a0953bd9f0d
laminemessaci <lamine.messaci18@gmail.com>
2022-11-11 18:28:12 +0100

    Now BlogPost detail with params Id page don't need usEffect, useState , loadin..

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
+import PostDetailPage, { loader as blogPostLoader } from './pages/PostDetail';
 import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
@@ -18,7 +18,11 @@ const router = createBrowserRouter(
       <Route index path="/" element={<WelcomePage />} />
       <Route path="/blog" element={<BlogLayout />}>
         <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-        <Route path=":id" element={<PostDetailPage />} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+        />
       </Route>
       <Route path="/blog/new" element={<NewPostPage />} />
     </Route>
